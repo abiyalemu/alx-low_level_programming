6d3c383866b82e44d1a7d03c3272d99dc0b876da
root <abiya.hiwot@gmail.com>
2021-10-13 08:11:53 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,52 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - Entry Point
- * @head: head
- * @idx: index
- * @n: data
- * Return: 0
+ * insert_nodeint_at_index - adds a new node at a given position in a list
+ * @head: linked list of type listint_t
+ * @idx: position to add element at
+ * @n: element to add to list
+ *
+ * Return: address of new node, or NULL if function fails
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	listint_t *new, *current;
+	unsigned int c_i = 0;
 
-	listint_t *new;
-	listint_t *temp;
-
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	temp = *head;
+	new = malloc(sizeof(listint_t));
 
-	while (temp != NULL)
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
 	{
-		if (count == idx - 1)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			else
-			{
-				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
-		count++;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	return (NULL);
+
+	current = *head;
+
+	while (c_i < idx - 1)
+	{
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+
+		current = current->next;
+		c_i++;
+	}
+
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
 }
