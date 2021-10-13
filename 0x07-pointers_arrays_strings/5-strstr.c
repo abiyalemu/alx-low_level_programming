d97625f2469004cd95981142e2d28dc9d0e3a8c3
root <abiya.hiwot@gmail.com>
2021-10-13 11:36:56 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurence of a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return:  pointer to location in string that substring begins
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *start, *p, *t;
+
+	start = haystack;
+
+	while (*start != 0)
+	{
+		p = start;
+		t = needle;
+
+		if (*t == 0)
+			return (start);
+
+		while (*p != 0)
+		{
+			if (*t != *p)
+				break;
+			t++;
+			p++;
+			if (*t == 0)
+				return (start);
+		}
+
+		start++;
+	}
+
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sums of the diagonals of a
+ * square matrix of integers
+ * @a: matrix
+ * @size: size each side of matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum_1 = 0;
+	int sum_2 = 0;
+	int index_1 = 0;
+	int index_2 = 1;
+
+	while (index_1 < size)
+	{
+		sum_1 += a[(size + 1) * index_1];
+		index_1++;
+	}
+
+	while (index_2 <= size)
+	{
+		sum_2 += a[(size - 1) * index_2];
+		index_2++;
+	}
+
+	printf("%i, %i\n", sum_1, sum_2);
+}
