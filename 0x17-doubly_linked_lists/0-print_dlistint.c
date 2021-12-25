6fa244fe964cdc96799bcd6b47b007cc1cf27e1f
root <abiya.hiwot@gmail.com>
2021-12-25 00:17:24 -0800

    printing all elemnts

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,27 +1,19 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_dlistint_rec - prints all the elements of a dlistint_t list.
- * @h: double linked list.
- * @elements: number of nodes.
- * Return: elements value.
- */
-size_t print_dlistint_rec(const dlistint_t *h, size_t elements)
-{
-	if (h)
-	{
-		printf("%i\n", h->n);
-		elements = print_dlistint_rec(h->next, elements + 1);
-	}
-	return (elements);
-}
-
-/**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: double linked list.
- * Return: number of nodes.
+ *print all the element ofa dlistint_t list by print_dlistint
+ *@h: pointer to the start of the linked list
+ *return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	return (print_dlistint_rec(h, 0));
+  size_t i;
+
+  for (i = 0; h != NULL; i++)
+    {
+      printf("%d\n", h->n);
+      h = h->next;
+    }
+  return (i);
 }
