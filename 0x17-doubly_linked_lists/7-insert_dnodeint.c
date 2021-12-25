92b89ccc194f0782969ccecd2384a4088e62575e
root <abiya.hiwot@gmail.com>
2021-12-25 02:52:50 -0800

    inserting new node at given  index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - function with three arguments
- * @h: pointer to doubly linked list
- * @idx: index position to insert node
- * @n: value of new node
+ * insert_dnodeint_at_index - inserts a new node at a given index
+ *@h: double pointer to the beginning of the  linked list
+ *@idx: index at which to inser the new node
+ *@: data to enter into new node
  *
- * Description: inserting new node into a doubly linked list
- * Return: address of new node or NULL if failed
+ *return: pointer to the new node, or null on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *cursor;
-
-	if (h == NULL)
+  dlistint_t *new_node, *next, *val;
+  unsigned int i;
+  if (h == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	/* loop until nth node of idx */
-	cursor = *h;
-	while (idx > 1 && cursor && cursor->next)
+  if (idx != 0)
 	{
-		cursor = cursor->next;
-		idx--;
+		val = *h;
+		for (i = 0; i < idx - 1 && val != NULL; i++)
+			val = val->next;
+		if (val == NULL)
+			return (NULL);
 	}
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+        new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	if (idx > 1 || cursor == NULL)
-		return (NULL);
-
-	/* assign value to new node */
-	new->n = n;
-
-	/* insert node */
-	if (cursor->next != NULL)
-		cursor->next->prev = new;
-	new->prev = cursor;
-	new->next = cursor->next;
-	cursor->next = new;
-
-	return (new);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		next = *h;
+		*h = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		new_node->prev = val;
+		next = val->next;
+		val->next = new_node;
+	}
+	new_node->next = next;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+	return (new_node);
 }
+  
